592d818f6acdef0b89de819f65842b4bb179c069
HenriqueRf <47756334+HenriqueRf@users.noreply.github.com>
2025-10-20 08:57:27 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "lambda_function_name" {
   description = "Name of the deployed Lambda function"
   value       = aws_lambda_function.hello_world.function_name
 }
+
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket that triggers the Lambda"
+  value       = aws_s3_bucket.event_bucket.bucket
+}
