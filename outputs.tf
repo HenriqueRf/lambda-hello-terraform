fd996558f3d84d89beefa18a433f38aec41af6e6
HenriqueRf <47756334+HenriqueRf@users.noreply.github.com>
2025-10-30 09:55:38 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "lambda_function_name" {
-  description = "Name of the deployed Lambda function"
+  description = "Nome da função Lambda criada"
   value       = aws_lambda_function.hello_world.function_name
 }
 
 output "s3_bucket_name" {
-  description = "Name of the S3 bucket used to store Lambda logs"
+  description = "Nome do bucket S3 onde os registros são armazenados"
   value       = aws_s3_bucket.lambda_records.bucket
 }
