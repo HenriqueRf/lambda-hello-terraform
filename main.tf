cdf742edc188fd3f26f6b6be4c56233ba4a36eea
HenriqueRf <47756334+HenriqueRf@users.noreply.github.com>
2025-10-30 09:54:39 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -139,16 +139,3 @@ resource "aws_lambda_permission" "allow_eventbridge" {
   source_arn    = aws_cloudwatch_event_rule.lambda_schedule.arn
 }
 
-# =============================
-# OUTPUTS
-# =============================
-
-output "lambda_function_name" {
-  description = "Nome da função Lambda criada"
-  value       = aws_lambda_function.hello_world.function_name
-}
-
-output "s3_bucket_name" {
-  description = "Nome do bucket S3 onde os registros são armazenados"
-  value       = aws_s3_bucket.lambda_records.bucket
-}
